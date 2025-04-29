d54a8163e8c8db45bbdfdc94b6dc34079213c10c
Alan <alanzheng334@gmail.com>
2025-04-29 14:48:33 -0400

    changed seed files to ts files

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          It just couldn't be... Only a fool would say that...
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
